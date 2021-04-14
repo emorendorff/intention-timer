4349f7a5b71a98cc2f8b8a7b0ac3b5a0722ace3d
Em Orendorff <em.orendorff@gmail.com>
2021-04-14 12:20:55 -0500

    Create activity class constructor

---
+++ Activity.js
@@ -0,0 +1,15 @@
+Class Activity {
+    constructor(activityCat, descriptionInput, minutesInput, secondsInput){
+        this.category = activityCat;
+        this.description = descriptionInput;
+        this.minutes = minutesInput;
+        this.seconds = secondsInput;
+    }
+      }
+    startTimer() {
+    }
+    markComplete() {
+    }
+    saveToStorage() {
+    }
+}
