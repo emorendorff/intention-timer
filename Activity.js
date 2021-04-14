9d1ca07d7252e893a57e7258f1d4c816ae229468
Em Orendorff <em.orendorff@gmail.com>
2021-04-14 12:59:25 -0500

    Began add timer functionality

--- Activity.js
+++ Activity.js
@@ -7,7 +7,33 @@ Class Activity {
     }
       }
     startTimer() {
-      
+      var userStartTime = (this.minutes * 60) + (this.seconds);
+
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var timeDifference = userStartTime - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (timeDifference < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+      }, 1000);
     }
     markComplete() {
     }
@@ -27,7 +53,7 @@ Class Activity {
 //   // Get today's date and time
 //   var now = new Date().getTime();
 //
-//   // Find the distance between now and the count down date
+//   // Find the timeDifference between now and the count down date
 //   var distance = countDownDate - now;
 //
 //   // Time calculations for days, hours, minutes and seconds
